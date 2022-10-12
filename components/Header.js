56adf7f8e6776006ba2fbd31897abcae1a1a9975
ardaege355 <86205217+ardaege355@users.noreply.github.com>
2022-10-12 19:45:54 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,7 @@ const navItems = [
     { icon: 'fal fa-home', active: 'fa fa-home', label: 'Home', href: '/' },
     { icon: 'fal fa-compass', active: 'fa fa-compass', label: 'Projects', href: '/projects' },
     { icon: 'fal fa-phone', active: 'fa fa-phone', label: 'Contact', href: '/contact' },
-    { icon: 'fal fa-rss', active: 'fa fa-rss', label: 'Blogs', href: 'https://blog.umut.live' }
+    { icon: 'fal fa-rss', active: 'fa fa-rss', label: 'Blogs', href: 'https://reactsite.blogspot.com/2022/10/kendinize-ait-kisisel-websitesi.html?m=1' }
 ]
 
 import Link from 'next/link';
